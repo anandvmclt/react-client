91f3facbafc9829d336bb9a07d766e65b4203b75
Anand VM <anandnambissan2@gmail.com>
2022-05-24 08:32:15 +0530

    Video 23 compleeted

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -1,10 +1,20 @@
-const addTocartReducer = (state={cartItems : []}, action) => {
+const addTocartReducer = (state = { cartItems: [] }, action) => {
 
-    switch(action.type){
-        case 'ADD_TO_CART': return {
-            ...state,cartItems:[...state.cartItems, action.payload]
-        }
-        default : return state
+    switch (action.type) {
+        case 'ADD_TO_CART':
+
+            const alreadyexist = state.cartItems.find(item => item._id == action.payload._id)
+            if (alreadyexist) {
+                return {
+                    ...state, cartItems: state.cartItems.map(item =>
+                        item._id == action.payload._id ? action.payload : item)
+                }
+            } else {
+                return {
+                    ...state, cartItems: [...state.cartItems, action.payload]
+                }
+            }
+        default: return state
     }
 }
 
